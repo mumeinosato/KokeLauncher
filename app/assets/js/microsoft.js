9f61c8dd932b327d8298bd22d615f05566d9cfed
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-05-17 20:34:15 +0900

    修正

--- app/assets/js/microsoft.js
+++ app/assets/js/microsoft.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const request = require('request')
 
 // Constants
-const clientId = 'a933ebf4-788c-4eea-b03a-48f14985f2ad'
+const clientId = 'ce9c7ade-7cee-4c4c-83bc-0c0edafdcaea'
 
 const tokenUri = 'https://login.microsoftonline.com/consumers/oauth2/v2.0/token'
 const authXBLUri = 'https://user.auth.xboxlive.com/user/authenticate'
